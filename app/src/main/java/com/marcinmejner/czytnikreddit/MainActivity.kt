0fb079c5c95121f9bbe66ece333f50f6bb75aa3f
Marcin Mejner <marczu09@gmail.com>
2018-04-27 21:24:12 +0200

    commit 4 budowanie layoutu dla caardView

--- app/src/main/java/com/marcinmejner/czytnikreddit/MainActivity.kt
+++ app/src/main/java/com/marcinmejner/czytnikreddit/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import com.marcinmejner.czytnikreddit.model.Feed
+import com.marcinmejner.czytnikreddit.model.Post
 import com.marcinmejner.czytnikreddit.model.entry.Entry
 import com.marcinmejner.czytnikreddit.utils.BASE_URL
 import com.marcinmejner.czytnikreddit.utils.ExtractXML
@@ -34,12 +35,14 @@ class MainActivity : AppCompatActivity() {
             override fun onResponse(call: Call<Feed>, response: Response<Feed>) {
 
                 val entrys: List<Entry>? = response.body()?.entrys
-//                Log.d(TAG, "onResponse: entrys: " + response.body()?.entrys)
+                Log.d(TAG, "onResponse: entrysss: " + response.body()?.entrys)
 
 //                Log.d(TAG, "onResponse: author name: " + entrys?.get(0)?.author)
 //                Log.d(TAG, "onResponse: updated: " + entrys?.get(0)?.updated)
 //                Log.d(TAG, "onResponse: title: " + entrys?.get(0)?.title)
 
+
+                var posts = ArrayList<Post>()
                 for (i in 0 until entrys!!.size) {
                     val extractXml1 = ExtractXML(entrys[i].content!!, "<a href=")
                     var postContent: ArrayList<String> = extractXml1.start()
@@ -51,34 +54,30 @@ class MainActivity : AppCompatActivity() {
                     } catch (e: KotlinNullPointerException) {
                         Log.d(TAG, "onResponse: NullPointerException (thumbnail) : ${e.localizedMessage}")
                         postContent.add("")
-                    }catch (e: IndexOutOfBoundsException){
+                    } catch (e: IndexOutOfBoundsException) {
                         postContent.add("tunicniema")
                         Log.d(TAG, "onResponse: IndexOutOfBoundsException (thumbnail) : ${e.localizedMessage}")
 
                     }
-
+                    val lastPosition = postContent.size -1
+                    posts.add(Post(
+                            entrys[i].title!!,
+                            entrys[i].author!!.name!!,
+                            entrys[i].updated!!,
+                            postContent[0],
+                            postContent[lastPosition]
+                    ))
                     Log.d(TAG, "onResponse: tralalala : $postContent")
 
-
                 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                for(j in 0 until posts.size){
+                    Log.d(TAG, "onResponse dane z XMLa: \n" +
+                            "PostURL : ${posts[j].postURL} \n" +
+                            "ThumbnailUrl : ${posts[j].thumbnailURL} \n" +
+                            "Title : ${posts[j].title} \n" +
+                            "Author : ${posts[j].author} \n" +
+                            "Date Updated : ${posts[j].date_updated}")
+                            }
             }
 
             override fun onFailure(call: Call<Feed>, t: Throwable) {
---
+++ app/src/main/java/com/marcinmejner/czytnikreddit/test.java
@@ -0,0 +1,17 @@
+package com.marcinmejner.czytnikreddit;
+
+import android.util.Log;
+
+import com.marcinmejner.czytnikreddit.utils.ExtractXML;
+
+import java.util.List;
+
+public class test {
+    private static final String TAG = "test";
+
+
+
+
+
+
+}
